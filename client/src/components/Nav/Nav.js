701b73666b7bf5c11469b9abf1deca4f233c02c2
Alicia <chan.aliciaw@gmail.com>
2018-11-15 13:43:46 -0500

    temp navbar

---
+++ client/src/components/Nav/Nav.js
@@ -0,0 +1,47 @@
+// import { Link } from "react-router-dom";
+import React, { Component } from 'react'
+import { Menu, Segment } from 'semantic-ui-react'
+
+export default class MenuExampleSecondaryPointing extends Component {
+  state = { activeItem: 'home' }
+
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+
+  render() {
+    const { activeItem } = this.state
+
+    return (
+      <div>
+        <Menu pointing secondary>
+          <Menu.Item name='Home' active={activeItem === 'Home'} onClick={this.handleItemClick} />
+          <Menu.Item
+            name='Login'
+            active={activeItem === 'login'}
+            onClick={this.handleItemClick}
+          />
+          <Menu.Item
+            name='Register'
+            active={activeItem === 'register'}
+            onClick={this.handleItemClick}
+          />
+           <Menu.Item
+            name='Dashboard'
+            active={activeItem === 'dashboard'}
+            onClick={this.handleItemClick}
+          />
+          <Menu.Menu position='right'>
+            <Menu.Item
+              name='logout'
+              active={activeItem === 'logout'}
+              onClick={this.handleItemClick}
+            />
+          </Menu.Menu>
+        </Menu>
+
+        <Segment>
+          <img src='/images/wireframe/media-paragraph.png' />
+        </Segment>
+      </div>
+    )
+  }
+}
