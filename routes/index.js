72a6e66045d92e2e9617466abbde48bdf6bb84bc
Luis Rishi Puno <LRPuno@gmail.com>
2018-11-15 10:54:22 -0500

    This Branch to Work

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,13 @@
 const path = require("path");
 const router = require("express").Router();
-const apiRoutes = require("./api");
+const bubble = require('./bubble');
 
-// API Routes
-router.use("/api", apiRoutes);
+// Bubble and Post Routes
+router.use("/bubbles/:id", bubble);
 
 // If no API routes are hit, send the React app
-router.use(function(req, res) {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
-});
+router.use((req, res) =>
+  res.sendFile(path.join(__dirname, "../client/public/index.html"))
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,22 +1,39 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const application = express();
+const express = require("express");
+const logger = require('morgan');
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const routes = require("./routes");
+const app = express();
+const PORT = process.env.PORT || 3001;
 
-const PORT = process.env.PORT || 8080;
-const db = require('./models');
+// Use morgan logger for logging requests
+app.use(logger("dev"));
+// Configure body parsing for AJAX requests
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-application.use(bodyParser.urlencoded({ extended: true }));
-application.use(bodyParser.json());
-
+/* ////////////////////////////////////////////////Production///////////////////////////////////////
+// Serve up static assets
 if (process.env.NODE_ENV === "production") {
-  application.use(express.static("client/build"));
+  app.use(express.static("client/build"));
 }
+*/
 
-application.use(require('./routes'));
+app.use(express.static("client/public"));
 
-db.sequelize.sync({ force: true })
-  .then(function() {
-    application.listen(PORT, function() {
-      console.log("App is listening on port", PORT);
-    })
-  });
+// Add routes, both API and view
+app.use(routes);
+
+// Connect to the Mongo DB
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/bubbles",
+  {
+    useCreateIndex: true,
+    useNewUrlParser: true
+  }
+);
+
+// Start the API server
+app.listen(PORT, () =>
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`)
+);
