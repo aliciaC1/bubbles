b40aa39b7a122edccd66138c84e99fb04f89f881
Luis Rishi Puno <LRPuno@gmail.com>
2018-11-18 13:51:02 -0500

    Fixed the Posts But Still Need Bubble ID

--- controller/post.js
+++ controller/post.js
@@ -14,8 +14,25 @@ module.exports = {
     */
 
     db.User.findOne({ sessionID: req.cookies.sessionID })
-      .then(function (SpeficiUser) {
-
+      .then(function (SpecificUser) {
+        const userName = SpecificUser.username;
+        const post = new Post(req.body);
+        post.setUserId(userName);
+        db.Post.create(post)
+          .then(function (dbPost) {
+            return db.Bubble.findOneAndUpdate({ _id: req.params.bubbleid }, { $push: { post: dbPost._id } }, { new: true });
+          }).then(function (dbPost) {
+            // If we were able to successfully update an Bubble, send it back to the client
+            /*
+            1. From here find the user in the collection and put his name into the post. 
+            2. Probably along the lienes of "db.User.findOne({req.params.user})= to find his username to display"
+            */
+            res.json(dbPost);
+          })
+          .catch(function (err) {
+            // If an error occurred, send it to the client
+            res.json(err);
+          });
       })
   }
   // Delete a note with a given id
