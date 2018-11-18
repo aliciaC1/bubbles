b1f1989191b46332b9bb2293cd1bc5d4cb27c11a
Luis Rishi Puno <LRPuno@gmail.com>
2018-11-18 13:34:32 -0500

    Make sure nothing is broken

--- controller/post.js
+++ controller/post.js
@@ -8,29 +8,15 @@ module.exports = {
   // Create a new note
   create: function (req, res) {
 
-    const post = new Post(req.body);
-    post.setBubbleId(req.params.bubbleid);
-    post.setUserID(req.params.user);
+    /*
 
-    db.Post.create(post)
-      .then(function (dbPost) {
-        // If a post was created successfully, find one Bubble with an `_id` equal to `req.params.id`. Update the Bubble to be associated with the new post
-        // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
-        // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
-        return db.Bubble.findOneAndUpdate({ _id: req.params.bubbleid }, { $push: { post: dbPost._id } }, { new: true });
+      1. Need a variable that allows me to grab the bubble id. 
+    */
+
+    db.User.findOne({ sessionID: req.cookies.sessionID })
+      .then(function (SpeficiUser) {
+
       })
-      .then(function (dbPost) {
-        // If we were able to successfully update an Bubble, send it back to the client
-        /*
-        1. From here find the user in the collection and put his name into the post. 
-        2. Probably along the lienes of "db.User.findOne({req.params.user})= to find his username to display"
-        */
-        res.json(dbPost);
-      })
-      .catch(function (err) {
-        // If an error occurred, send it to the client
-        res.json(err);
-      });
   }
   // Delete a note with a given id
   /*
