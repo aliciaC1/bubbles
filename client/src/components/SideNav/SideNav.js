0b7c3dc71281fe25c4e38ef4dbdfd0d23cc884aa
Alicia <chan.aliciaw@gmail.com>
2018-11-19 20:06:17 -0500

    added selection to side nav

--- client/src/components/SideNav/SideNav.js
+++ client/src/components/SideNav/SideNav.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react'
-import { Button, Header, Icon, Image, Menu, Segment, Sidebar } from 'semantic-ui-react'
+import { Button, Header, Icon, Image, Menu, Segment, Sidebar, Popup } from 'semantic-ui-react'
+import BubbleColor from '../BubbleColor';
+import AddBubbleModal from '../AddBubbleModal';
 
-export default class SidebarExampleSidebar extends Component {
+export default class SideNavSidebar extends Component {
   state = { visible: false }
 
   handleHideClick = () => this.setState({ visible: false })
@@ -14,11 +16,9 @@ export default class SidebarExampleSidebar extends Component {
     return (
       <div>
         <Button.Group>
-          <Button disabled={visible} onClick={this.handleShowClick}>
-            Show sidebar
-          </Button>
-          <Button disabled={!visible} onClick={this.handleHideClick}>
-            Hide sidebar
+          <Button disabled={visible} onClick={this.handleShowClick}>            
+          <Icon loading size='big' name='circle notch' />
+          {/* <Icon name='sliders horizontal' /> */}
           </Button>
         </Button.Group>
 
@@ -31,26 +31,82 @@ export default class SidebarExampleSidebar extends Component {
             onHide={this.handleSidebarHide}
             vertical
             visible={visible}
-            width='thin'
+            width='wide'
           >
             <Menu.Item as='a'>
-              <Icon name='home' />
-              Home
+            <div>
+              <Image src='https://react.semantic-ui.com/images/wireframe/square-image.png' avatar size='tiny' />
+              <span size = "small">Username</span>
+            </div>
             </Menu.Item>
             <Menu.Item as='a'>
-              <Icon name='gamepad' />
-              Games
+            <AddBubbleModal></AddBubbleModal>
+              <Icon loading size='big' name='spinner'/>
+              Create New Bubble
             </Menu.Item>
             <Menu.Item as='a'>
-              <Icon name='camera' />
-              Channels
+              <Popup 
+                trigger={<Icon loading size='huge' name='circle notch'/>} 
+                flowing
+                position='right center' 
+                on='click'>
+                  <BubbleColor></BubbleColor>
+              </Popup>
+              Coolest
+            </Menu.Item>
+            <Menu.Item as='a'>
+              <Popup 
+                trigger={<Button>Pretty Cool  <Icon loading size='big' name='circle notch'/></Button>} 
+                flowing
+                position='right center' 
+                on='click'>
+                  <BubbleColor></BubbleColor>
+              </Popup>
+            </Menu.Item>
+            <Menu.Item as='a'>
+              <Popup 
+                trigger={<Button>Plain Cool  <Icon loading size='big' name='circle notch'/></Button>} 
+                flowing
+                position='right center' 
+                on='click'>
+                  <BubbleColor></BubbleColor>
+              </Popup>
+            </Menu.Item>
+            <Menu.Item as='a'>
+              <Popup 
+                trigger={<Button>Uncool  <Icon loading size='big' name='circle notch'/></Button>} 
+                flowing
+                position='right center' 
+                on='click'>
+                  <BubbleColor></BubbleColor>
+              </Popup>
+            </Menu.Item>
+            <Menu.Item as='a'>
+            <BubbleColor></BubbleColor> LAME
+            </Menu.Item>
+            <Menu.Item as='a'>
+            <Icon loading size='big' name='x' /> 
+            Logout       
             </Menu.Item>
           </Sidebar>
 
           <Sidebar.Pusher>
-            <Segment basic>
-              <Header as='h3'>Application Content</Header>
-              <Image src='https://react.semantic-ui.com/images/wireframe/paragraph.png' />
+          <Segment basic
+              textAlign='center'
+              style={{ minHeight: 1350, padding: '1em 0em' }}
+              vertical
+              size = 'huge'
+            >
+              <div className='bubbleArea'>
+              {/* <Icon.Group size='huge'> */}
+                <Icon loading size='massive' name='circle notch' />
+                {/* <Icon name='user' /> */}
+              {/* </Icon.Group> */}
+              </div>
+                 
+            {/* <Segment basic>
+              <Header as='h3'></Header>
+              <Image src='https://react.semantic-ui.com/images/wireframe/paragraph.png' /> */}
             </Segment>
           </Sidebar.Pusher>
         </Sidebar.Pushable>
