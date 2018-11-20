f0b7dfe9633fa2ca09b60e89103c3c58542647bd
Alicia <chan.aliciaw@gmail.com>
2018-11-19 20:06:00 -0500

    update new bubble form

--- client/src/components/FormBubble/FormBubble.js
+++ client/src/components/FormBubble/FormBubble.js
@@ -1,53 +1,65 @@
-import React from 'react'
-import { Form, Input } from 'semantic-ui-react'
+import React, { Component }from 'react'
+import { Form, Input,Radio, Button } from 'semantic-ui-react'
 
-const FormExampleRequiredField = () => (
-  <Form>
-    <Form.Field required>
-      <label>Bubble Name</label>
-      <Input placeholder='Bubble Name' />
-    </Form.Field>
-    <Form.Group inline>
-          <label>Bubble Rank (1 : Coolest | 5 : Uncoolest)</label>
-          <Form.Field
-            control={Radio}
-            label='Coolest'
-            value='1'
-            checked={value === '1'}
-            onChange={this.handleChange}
-          />
-          <Form.Field
-            control={Radio}
-            label='Pretty Cool'
-            value='2'
-            checked={value === '2'}
-            onChange={this.handleChange}
-          />
-          <Form.Field
-            control={Radio}
-            label='Cool'
-            value='3'
-            checked={value === '3'}
-            onChange={this.handleChange}
-          />
-           <Form.Field
-            control={Radio}
-            label='Uncool'
-            value='4'
-            checked={value === '4'}
-            onChange={this.handleChange}
-          />
-           <Form.Field
-            control={Radio}
-            label='Lame'
-            value='5'
-            checked={value === '5'}
-            onChange={this.handleChange}
-          />
-        </Form.Group>
-  </Form>
-)
+class AddBubbleForm extends Component {
+  state = {}
 
-export default FormExampleRequiredField
+  handleChange = (e, { value }) => this.setState({ value })
+
+  render() {
+    const { value } = this.state
+    return(
+      <Form>
+      <Form.Field required>
+        <label>Bubble Name</label>
+        <Input placeholder='Bubble Name' />
+      </Form.Field>
+      <Form.Group required inline>
+            <label>Bubble Rank (1 : Coolest | 5 : Uncoolest) </label>
+            <Form.Field 
+              control={Radio}
+              label='Coolest'
+              value='1'
+              checked={value === '1'}
+              onChange={this.handleChange}
+            />
+            <Form.Field
+              control={Radio}
+              label='Pretty Cool'
+              value='2'
+              checked={value === '2'}
+              onChange={this.handleChange}
+            />
+            <Form.Field
+              control={Radio}
+              label='Cool'
+              value='3'
+              checked={value === '3'}
+              onChange={this.handleChange}
+            />
+             <Form.Field
+              control={Radio}
+              label='Uncool'
+              value='4'
+              checked={value === '4'}
+              onChange={this.handleChange}
+            />
+             <Form.Field
+              control={Radio}
+              label='Lame'
+              value='5'
+              checked={value === '5'}
+              onChange={this.handleChange}
+            />
+          </Form.Group>
+          <Form.Field control = {Button}>Add</Form.Field>
+    </Form>
+    );
+  }
+} 
+ 
+
+
+export default AddBubbleForm
 
 // missing submit button **** 
