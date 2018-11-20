e69b864a233aee71dde1df7ad8e0d4493260e7ff
Alicia <chan.aliciaw@gmail.com>
2018-11-20 01:12:19 -0500

    feed

--- client/src/components/Feed/Feed.js
+++ client/src/components/Feed/Feed.js
@@ -1,108 +1,52 @@
 import React from 'react'
-import { Feed, Icon } from 'semantic-ui-react'
+import PostText from '../PostText';
+import PostImage from '../PostImage';
+import PostForm from '../PostForm';
 
-const FeedExampleBasic = () => (
-  <Feed>
-    <Feed.Event>
-      <Feed.Label>
-        <img src='https://react.semantic-ui.com/images/avatar/small/elliot.jpg' />
-      </Feed.Label>
-      <Feed.Content>
-        <Feed.Summary>
-          <Feed.User>Elliot Fu</Feed.User> added you as a friend
-          <Feed.Date>1 Hour Ago</Feed.Date>
-        </Feed.Summary>
-        <Feed.Meta>
-          <Feed.Like>
-            <Icon name='like' />
-            4 Likes
-          </Feed.Like>
-        </Feed.Meta>
-      </Feed.Content>
-    </Feed.Event>
+import { Segment, Grid, Image, List, Divider } from 'semantic-ui-react'
 
-    <Feed.Event>
-      <Feed.Label image='/images/avatar/small/helen.jpg' />
-      <Feed.Content>
-        <Feed.Summary>
-          <a>Helen Troy</a> added <a>2 new illustrations</a>
-          <Feed.Date>4 days ago</Feed.Date>
-        </Feed.Summary>
-        <Feed.Extra images>
-          <a>
-            <img src='https://react.semantic-ui.com/images/wireframe/image.png' />
-          </a>
-          <a>
-            <img src='https://react.semantic-ui.com/images/wireframe/image.png' />
-          </a>
-        </Feed.Extra>
-        <Feed.Meta>
-          <Feed.Like>
-            <Icon name='like' />
-            1 Like
-          </Feed.Like>
-        </Feed.Meta>
-      </Feed.Content>
-    </Feed.Event>
+const FeedView = () => (
+  <Grid celled='internally'>
+    <Grid.Row>
+      <Grid.Column width={4}>
+          <List animated ordered verticalAlign='middle'>
+    <List.Item>
+      <Image avatar src='https://react.semantic-ui.com/images/avatar/small/helen.jpg' />
+      <List.Content> User 1
+      </List.Content>
+    </List.Item>
+    <List.Item>
+      <Image avatar src='https://react.semantic-ui.com/images/avatar/small/christian.jpg' />
+      <List.Content>
+        <List.Header>User 2
+        </List.Header>
+      </List.Content>
+    </List.Item>
+    <List.Item>
+      <Image avatar src='https://react.semantic-ui.com/images/avatar/small/daniel.jpg' />
+      <List.Content>
+        <List.Header>User 1
+        </List.Header>
+      </List.Content>
+    </List.Item>
+  </List>
+      </Grid.Column>
+      <Grid.Column width={12}>
+      <Segment padded>
+      <div className ="display">
+        <PostText></PostText>
+        <PostImage></PostImage>
+      </div>
+      <Divider horizontal/>
+      <div className = "PostArea">
+        <PostForm></PostForm>
+      </div>
+      </Segment>
+      
+      </Grid.Column>
+    </Grid.Row>
 
-    <Feed.Event>
-      <Feed.Label image='/images/avatar/small/jenny.jpg' />
-      <Feed.Content>
-        <Feed.Summary date='2 Days Ago' user='Jenny Hess' content='add you as a friend' />
-        <Feed.Meta>
-          <Feed.Like>
-            <Icon name='like' />
-            8 Likes
-          </Feed.Like>
-        </Feed.Meta>
-      </Feed.Content>
-    </Feed.Event>
-
-    <Feed.Event>
-      <Feed.Label image='/images/avatar/small/joe.jpg' />
-      <Feed.Content>
-        <Feed.Summary>
-          <a>Joe Henderson</a> posted on his page
-          <Feed.Date>3 days ago</Feed.Date>
-        </Feed.Summary>
-        <Feed.Extra text>
-          Ours is a life of constant reruns. We're always circling back to where we'd we started,
-          then starting all over again. Even if we don't run extra laps that day, we surely will
-          come back for more of the same another day soon.
-        </Feed.Extra>
-        <Feed.Meta>
-          <Feed.Like>
-            <Icon name='like' />
-            5 Likes
-          </Feed.Like>
-        </Feed.Meta>
-      </Feed.Content>
-    </Feed.Event>
-
-    <Feed.Event>
-      <Feed.Label image='/images/avatar/small/justen.jpg' />
-      <Feed.Content>
-        <Feed.Summary>
-          <a>Justen Kitsune</a> added <a>2 new photos</a> of you
-          <Feed.Date>4 days ago</Feed.Date>
-        </Feed.Summary>
-        <Feed.Extra images>
-          <a>
-            <img src='https://react.semantic-ui.com/images/wireframe/image.png' />
-          </a>
-          <a>
-            <img src='https://react.semantic-ui.com/images/wireframe/image.png' />
-          </a>
-        </Feed.Extra>
-        <Feed.Meta>
-          <Feed.Like>
-            <Icon name='like' />
-            41 Likes
-          </Feed.Like>
-        </Feed.Meta>
-      </Feed.Content>
-    </Feed.Event>
-  </Feed>
+  </Grid>
 )
 
-export default FeedExampleBasic
+export default FeedView
