e6c744a72d754bd6f002cc2ad0c8e8e3204ffe70
Alicia <chan.aliciaw@gmail.com>
2018-11-25 02:59:36 -0500

    cleaned up userAvatar

--- client/src/components/UserAvatar/UserAvatar.js
+++ client/src/components/UserAvatar/UserAvatar.js
@@ -4,8 +4,13 @@ import "./UserAvatar.css";
 
 const UserAvatar = () => (
   <div>
-    <Image src='https://react.semantic-ui.com/images/wireframe/square-image.png' avatar size ="tiny"/>
-    <Header>Username</Header> 
+    <Header as='h2'>
+      <Image circular src='https://react.semantic-ui.com/images/avatar/large/patrick.png' /> 
+      <Header.Content>
+      Patrick
+      {/* <Header.Subheader floated = 'right' ><Icon name='settings' textAlign='right'/></Header.Subheader> */}
+    </Header.Content>
+    </Header>
   </div>
 )
 
