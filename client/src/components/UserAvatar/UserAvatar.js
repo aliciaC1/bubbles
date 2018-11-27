c650373c2f2b915a46c79c6344f519c9b7809b66
Alicia <chan.aliciaw@gmail.com>
2018-11-27 00:11:57 -0500

    added user avatar

--- client/src/components/UserAvatar/UserAvatar.js
+++ client/src/components/UserAvatar/UserAvatar.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Image, Icon, Header} from 'semantic-ui-react';
+import { Image, Header} from 'semantic-ui-react';
 import "./UserAvatar.css";
 
 const UserAvatar = (props) => (
   <div>
     <Header as='h2'>
-      <Image circular src='' /> 
+      <Image circular src= {props.avatar} /> 
       <Header.Content>
       {props.username}
     </Header.Content>
