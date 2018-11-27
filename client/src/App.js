53edcc81e452ad4dca3f5566d08efc567989555c
Alicia <chan.aliciaw@gmail.com>
2018-11-27 00:28:37 -0500

    added store redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,27 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Provider } from 'react-redux';
 import Home from "./pages/Home/home.js";
 import Login from "./pages/Login/login.js";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
 import NoMatch from "./pages/NoMatch";
+
+// import Posts from './components/posts/Posts';
 import './App.css';
 
 const App = () => (
 <Router>
   <div>
-    <Switch>
-        <Route exact path = "/" component = {Home} />
-        <Route exact path ="/login" component={Login} />
-        <Route exact path ="/register" component={Register} />
-        <Route exact path ="/dashboard" component={Dashboard} />
-
-
-        <Route component={NoMatch} />
-    </Switch>
+    <Provider store = { store }>
+      <Switch>
+          <Route exact path = "/" component = {Home} />
+          <Route exact path ="/login" component={Login} />
+          <Route exact path ="/register" component={Register} />
+          <Route exact path ="/dashboard" component={Dashboard} />
+          <Route component={NoMatch} />
+      </Switch>
+    </Provider>
   </div>
 </Router>
 );
