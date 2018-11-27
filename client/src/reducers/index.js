5a0a9e860b1ccf6117c7c59a25269f7bb7db6b6b
Alicia <chan.aliciaw@gmail.com>
2018-11-27 00:29:01 -0500

    auth reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,11 +1,11 @@
 import { combineReducers } from 'redux';
-// import authReducer from './authReducer';
+import authReducer from './authReducer';
 import errorReducer from './errorReducer';
 import profileReducer from './profileReducer';
 import postReducer from './postReducer';
 
 export default combineReducers({
-  // auth: authReducer,
+  auth: authReducer,
   errors: errorReducer,
   profile: profileReducer,
   post: postReducer
