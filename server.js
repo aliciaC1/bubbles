34c745dffe2db3037f5be74ba7bcd80738d1250d
Alicia <chan.aliciaw@gmail.com>
2018-11-28 03:38:05 -0500

    update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const logger = require('morgan');
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const routes = require("./routes");
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 3002;
 const path = require ('path');
 
 
@@ -17,18 +17,20 @@ app.use(bodyParser.json());
 // DB Config 
 const db = require ('./config/keys').mongoURI;
 
+app.use(express.static("client/build"));
+
 /* ////////////////////////////////////////////////Production///////////////////////////////////////
 */
 
 // Server static assets if in production 
-if (process.env.NODE_ENV === 'production') {
-  //Set static folder
-  app.use(express.static("client/build"));
+// if (process.env.NODE_ENV === 'production') {
+//   //Set static folder
+//   // app.use(express.static("client/build"));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+//   });
+// }
 
 // Add routes, both API and view
 app.use(routes);
