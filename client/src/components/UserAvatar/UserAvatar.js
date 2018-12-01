58e2afe61c15c37d88324ed22e9471f7c99b24fe
Alicia Chan <37819906+aliciaC1@users.noreply.github.com>
2018-12-01 09:42:23 -0500

    Update UserAvatar.js

--- client/src/components/UserAvatar/UserAvatar.js
+++ client/src/components/UserAvatar/UserAvatar.js
@@ -5,7 +5,7 @@ import "./UserAvatar.css";
 const UserAvatar = (props) => (
   <div>
     <Header as='h2'>
-      <Image circular src='' /> 
+      <Image circular src='https://cdn1.iconfinder.com/data/icons/user-pictures/100/female1-512.png' /> 
       <Header.Content>
       {props.username}
     </Header.Content>
